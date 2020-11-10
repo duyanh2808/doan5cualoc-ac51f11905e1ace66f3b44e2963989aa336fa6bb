a1929671379fbbf09805c8b91fbd79c8534bf376
duyanh2808 <70557142+duyanh2808@users.noreply.github.com>
2020-11-10 11:36:44 +0700

    first commit

---
+++ doan5cualoc-ac51f11905e1ace66f3b44e2963989aa336fa6bb/src/app/loai/loai.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoaiComponent } from './loai.component';
+
+describe('LoaiComponent', () => {
+  let component: LoaiComponent;
+  let fixture: ComponentFixture<LoaiComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoaiComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoaiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ doan5cualoc-ac51f11905e1ace66f3b44e2963989aa336fa6bb/src/app/loai/loai.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loai',
+  templateUrl: './loai.component.html',
+  styleUrls: ['./loai.component.css']
+})
+export class LoaiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
